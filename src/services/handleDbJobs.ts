4e98e2a21ac4769d9c81c0a6e1d388abb1d0e59b
Tomas Lugo <lugoleandro@hotmail.com>
2024-04-19 18:57:29 -0300

    full backend with new url

--- src/services/handleDbJobs.ts
+++ src/services/handleDbJobs.ts
@@ -1,11 +1,19 @@
 import type { JobColumns } from '@/types/types'
 import axios from 'axios'
-const url = 'http://localhost:5000/api'
 
+/**
+ * Retrieves jobs from the database based on the provided email.
+ *
+ * @param {string} email - The email of the user.
+ * @return {Promise<JobColumns[] | []>} - A promise that resolves to an array of JobColumns or an empty array.
+ */
 export const getJobsFromDB = async (
   email: string,
 ): Promise<JobColumns[] | []> => {
-  const res = await axios.get(url, {
+  if (typeof process.env.NEXT_PUBLIC_BACKEND_URL !== 'string') {
+    throw new Error('Wrong url provided, please contact support')
+  }
+  const res = await axios.get(process.env.NEXT_PUBLIC_BACKEND_URL, {
     params: {
       email,
     },
@@ -18,10 +26,22 @@ export const getJobsFromDB = async (
   return res.data.data
 }
 
+/**
+ * Adds a new job to the database.
+ *
+ * @param {JobColumns} newJobToSave - The job data to be saved.
+ * @return {Promise<JobColumns>} - A promise that resolves to the newly created job.
+ */
 export const addJobInDB = async (
   newJobToSave: JobColumns,
 ): Promise<JobColumns> => {
-  const res = await axios.post(url, { job: newJobToSave })
+  if (typeof process.env.NEXT_PUBLIC_BACKEND_URL !== 'string') {
+    throw new Error('Wrong url provided, please contact support')
+  }
+
+  const res = await axios.post(process.env.NEXT_PUBLIC_BACKEND_URL, {
+    job: newJobToSave,
+  })
 
   if (res.status !== 200) {
     throw new Error('Something went wrong saving job, try again later')
@@ -30,10 +50,23 @@ export const addJobInDB = async (
   return newJobToSave
 }
 
+/**
+ * Updates a job in the database with the given new job data.
+ *
+ * @param {JobColumns} newJobToSave - The new job data to be saved.
+ * @return {Promise<JobColumns>} - A promise that resolves to the updated job.
+ * @throws {Error} - If there is an error updating the job, with the message 'Something went wrong updating job, try again later'.
+ */
 export const updateJobInDB = async (
   newJobToSave: JobColumns,
 ): Promise<JobColumns> => {
-  const res = await axios.put(url, { job: newJobToSave })
+  if (typeof process.env.NEXT_PUBLIC_BACKEND_URL !== 'string') {
+    throw new Error('Wrong url provided, please contact support')
+  }
+
+  const res = await axios.put(process.env.NEXT_PUBLIC_BACKEND_URL, {
+    job: newJobToSave,
+  })
 
   if (res.status !== 200) {
     throw new Error('Something went wrong updating job, try again later')
@@ -42,8 +75,18 @@ export const updateJobInDB = async (
   return newJobToSave
 }
 
+/**
+ * Deletes a job from the database.
+ *
+ * @param {string} jobId - The ID of the job to be deleted.
+ * @return {Promise<string>} The ID of the deleted job.
+ */
 export const deleteJobInDB = async (jobId: string): Promise<string> => {
-  const res = await axios.delete(url, {
+  if (typeof process.env.NEXT_PUBLIC_BACKEND_URL !== 'string') {
+    throw new Error('Wrong url provided, please contact support')
+  }
+
+  const res = await axios.delete(process.env.NEXT_PUBLIC_BACKEND_URL, {
     params: {
       jobId,
     },
